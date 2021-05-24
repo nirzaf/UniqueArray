e4d893108fb0a44c1edcfad67ecab9df414735a4
Quadrate Tech Solutions <info@quadrate.lk>
2021-05-24 23:43:27 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace UniqueArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Numbers = { 4, 5, 3, 1, 6, 4, 2, 4, 5, 1, 2, 4, 5, 7, 8, 3 };
+
+            int[] Numbers2 = new int[10];
+
+            Console.WriteLine("Numbers Array ");
+            foreach (int num in Numbers)
+            {
+                Console.WriteLine(num.ToString());
+            }
+
+            foreach (int num in Numbers)
+            {
+
+                foreach (int num2 in Numbers2)
+                {
+                    if (num == num2)
+                    { 
+                       
+                    }
+                }           
+            }
+        }
+    }
+}
